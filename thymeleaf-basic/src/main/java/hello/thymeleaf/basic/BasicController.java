5739ee91dc5bc51ceef969e14abd1bd1a110f373
SeongIkKim <kpic1638@gmail.com>
2023-08-31 21:10:31 +0900

    example: thymeleaf basic objects

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,11 +1,13 @@
 package hello.thymeleaf.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -43,6 +45,22 @@ public class BasicController {
         model.addAttribute("users", list);
         model.addAttribute("userMap", map);
         return "basic/variables";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session){
+        // request, response, session, servletContext, locale같은 객체는 기본 객체로 지원
+        // Bean, request parameter 등의 편의객체도 지원
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    // 빈 등록
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
     }
 
     @Data
