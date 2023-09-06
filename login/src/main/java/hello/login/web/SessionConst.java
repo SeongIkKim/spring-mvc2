f5c9761cb6b80ce3ae32f35ceaf945161eddba35
SeongIkKim <kpic1638@gmail.com>
2023-09-07 04:55:51 +0900

    refactor: spring built-in session(jsession)

---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,8 @@
+package hello.login.web;
+
+public final class SessionConst {
+
+    private SessionConst(){}
+
+    public static final String LOGIN_MEMBER = "loginMember"; // JSessionID를 통해 얻어온 value(세션 저장소의 attributes 맵) 맵을 탐색할때 키로 사용할 string.
+}
